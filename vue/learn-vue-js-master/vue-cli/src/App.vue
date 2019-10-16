d4c7bf4aaecec9fbe8ad32f18d4fed7e20acbb88
yeGenieee <cyj95428@gmail.com>
2019-10-16 21:06:37 +0900

    [ADD] Use event emit in Single File Components

--- vue/learn-vue-js-master/vue-cli/src/App.vue
+++ vue/learn-vue-js-master/vue-cli/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
-    <app-header v-bind:propsdata="str"></app-header>
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
@@ -15,11 +17,16 @@ import AppHeader from './components/AppHeader.vue';
 export default {
   data: function() {
     return {
-      str: 'hi'
+      str: 'Header'
     }
   },
   components: {
     'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function()  {
+      this.str = 'hi'
+    }
   }
 }
 </script>
--- vue/learn-vue-js-master/vue-cli/src/components/AppHeader.vue
+++ vue/learn-vue-js-master/vue-cli/src/components/AppHeader.vue
@@ -3,13 +3,19 @@
       <h1>
           {{ propsdata }}
       </h1>
+      <button v-on:click="sendEvent">send</button>
     </header>
 </template>
 
 <script>
 export default {
     // 인스턴스 옵션
-    props: ['propsdata']
+    props: ['propsdata'],
+    methods: {
+        sendEvent: function() {
+             this.$emit('renew'); // 이벤트 올림
+        }
+    }
 
 }
 </script>
