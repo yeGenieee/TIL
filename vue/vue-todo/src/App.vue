cbdd59abe9aa81c8b63b9f9d97fb89f11972a765
yeGenieee <cyj95428@gmail.com>
2019-10-21 23:49:03 +0900

    [ADD] Vue.js Todo App

---
+++ vue/vue-todo/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <todo-header></todo-header>
+    <todo-input></todo-input>
+    <todo-list></todo-list>
+    <todo-footer></todo-footer>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoList from './components/TodoList.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+// // ES5
+// var my_cmp = {
+//   template: '<div>my component</div>'
+// };
+
+// new Vue({
+//   el: '',
+//   components: {
+//     '컴포넌트 이름': my_cmp
+//   }
+// });
+
+export default {
+  components: {
+    // 컴포넌트 태그명 : 컴포넌트 내용
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+
+}
+</script>
+
+<style>
+</style>
